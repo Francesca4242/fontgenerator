7a1cfa4a4beb127c4577198d7685ddaca2235430
Hello1024 <omattos@gmail.com>
2019-02-28 20:29:21 +0000

    Add font selector

--- js.js
+++ js.js
@@ -13,6 +13,7 @@ const loadImage = path =>
 document.getElementById('in_text').onchange = function(e) {
 	var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext("2d");
+	var font = document.getElementById('font').value;
 
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -21,7 +22,7 @@ document.getElementById('in_text').onchange = function(e) {
 		let thisXPos = xPos;
 		c = c.toUpperCase();
 		if (c>='A' && c <= 'Z')
-		  loadImage('font_src/'+c.toUpperCase()+'.jpg').then(i => ctx.drawImage(i, thisXPos, 0, 100, 100));
+		  loadImage('font_src/'+font+'/'+c.toUpperCase()+'.jpg').then(i => ctx.drawImage(i, thisXPos, 0, 100, 100));
 	    xPos += 100;
 	}
 };
